eaf3ce43bffa3b9e54d8d6e26e3e4e67242f25f8
eblyncho <eblyn52@naver.com>
2022-09-23 15:10:03 +0900

    정적 초기화 블록

---
+++ src/Tv.java
@@ -0,0 +1,9 @@
+public class Tv {
+    public static String company = "삼성";
+    public static String model = "LED 80";
+    public static String info;
+
+    static {
+        info = company + " - " + model;
+    }
+}
